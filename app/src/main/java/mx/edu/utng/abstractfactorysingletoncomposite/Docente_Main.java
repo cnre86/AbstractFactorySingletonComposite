3706672b14b63ff79dcc3f7bf91a487f4fb2c8fe
cnre86 <cnre86@gmail.com>
2016-04-21 00:40:27 -0500

    Initial commit

--- app/src/main/java/mx/edu/utng/abstractfactorysingletoncomposite/Docente_Main.java
+++ app/src/main/java/mx/edu/utng/abstractfactorysingletoncomposite/Docente_Main.java
@@ -1,7 +1,9 @@
 package mx.edu.utng.abstractfactorysingletoncomposite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by nayel on 14/04/2016.
@@ -12,5 +14,11 @@ public class Docente_Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.docente);
+        findViewById(R.id.bGuardar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Docente_Main.this, RegistrarMaterial.class));
+            }
+        });
     }
 }
--- app/src/main/java/mx/edu/utng/abstractfactorysingletoncomposite/Estudiante_Main.java
+++ app/src/main/java/mx/edu/utng/abstractfactorysingletoncomposite/Estudiante_Main.java
@@ -1,7 +1,9 @@
 package mx.edu.utng.abstractfactorysingletoncomposite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by nayel on 14/04/2016.
@@ -11,6 +13,12 @@ public class Estudiante_Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estudiante);
+        findViewById(R.id.bG).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Estudiante_Main.this, RegistrarMaterial.class));
+            }
+        });
     }
 
 
