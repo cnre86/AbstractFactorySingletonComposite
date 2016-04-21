d90910bfe81e4fafba8769f366351d1c648b0a60
cnre86 <cnre86@gmail.com>
2016-04-21 00:07:34 -0500

    R1

---
+++ app/src/main/java/mx/edu/utng/abstractfactorysingletoncomposite/RegistrarMaterial.java
@@ -0,0 +1,16 @@
+package mx.edu.utng.abstractfactorysingletoncomposite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nayel on 20/04/2016.
+ */
+public class RegistrarMaterial extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
